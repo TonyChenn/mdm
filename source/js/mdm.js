9669fb305320f23cbd183aa5b581d7f6d3c1d812
TonyChenn <852454151@qq.com>
2019-04-08 09:48:06 +0800

    mdm upload first time

---
+++ source/js/mdm.js
@@ -0,0 +1,33 @@
+$(function(){
+    var header_img_width=$(".header-box").height();
+    $(window).scroll(function(){
+        if($(window).scrollTop()>header_img_width-50){
+            $(".goTop").fadeIn(100);
+            $("#bar").removeClass("mdui-shadow-0")
+            $("#bar").addClass("mdui-color-indigo")
+        }else{
+            $(".goTop").fadeOut(100);
+            $("#bar").addClass("mdui-shadow-0");
+            $("#bar").removeClass("mdui-color-indigo")
+        }
+    });
+    //返回顶部点击事件
+    $(".goTop").click(function(){
+        $('body,html').animate({scrollTop:0},100);
+        return false;
+    });
+
+    function fix_footer_pos(){
+        $("footer").removeClass("fixed-bottom");
+        var contentHeight = document.body.scrollHeight,//网页正文全文高度
+            winHeight = window.innerHeight;//可视窗口高度，不包括浏览器顶部工具栏
+        if(!(contentHeight > winHeight)){
+            //当网页正文高度小于可视窗口高度时，为footer添加类fixed-bottom
+            $("footer").addClass("fixed-bottom");
+        } else {
+            $("footer").removeClass("fixed-bottom");
+        }
+    }
+    fix_footer_pos();
+    $(window).resize(fix_footer_pos);
+});
